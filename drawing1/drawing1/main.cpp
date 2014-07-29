da8ad49967e8ddc778cf495ae299821650501291
Константин Высоцкий <kostya19494@gmail.com>
2014-07-29 23:15:35 +0700

    Revert "shaders done, everything works fine"
    This reverts commit bd0003ec833243d6bf1f6868a82516d6943d2eb1.

--- drawing1/drawing1/main.cpp
+++ drawing1/drawing1/main.cpp
@@ -1,20 +1,7 @@
-
 #include <iostream>
-#include <stdio.h>
 #include <SDL.h>
 #include <glew.h>
-#include "LoadShaders.h"
-
 using namespace std;
-
-GLfloat* getScaleMatrix(GLfloat x, GLfloat y, GLfloat z){
-	GLfloat mat[] = {
-		x, 0.0, 0.0,
-		0.0, y, 0.0,
-		0.0, 0.0, z
-	};
-	return mat;
-}
 
 int main(int argc, char* argv[])
 {
@@ -27,42 +14,18 @@ int main(int argc, char* argv[])
 		SDL_Quit();
 		exit(EXIT_FAILURE);
 	}
-	GLint w, h;
-	SDL_GetWindowSize(window, &w, &h);
-	GLfloat aspect = GLfloat (w) / GLfloat(h);
-	//GLfloat* scale = getScaleMatrix(1.0, 1.0, 1.0);
-	GLfloat scale[] = {
-		1.0, 0.0, 0.0, 0.0,
-		0.0, aspect, 0.0, 0.0,
-		0.0, 0.0, 1.0, 0.0,
-		0.0, 0.0, 0.0, 1.0
-	};
 
 	GLuint vbuffer;
 	GLuint ebuffer;
 	GLfloat vertices[] =
-	{ -0.5, -0.5, -0.5, 1.0,
-	-0.5, 0.5, 0.5, 1.0,
-	0.5, -0.5, -0.5, 1.0,
-	-0.5, 0.5, 0.5, 1.0,
-	0.5, -0.5, -0.5, 1.0,
-	0.5, -0.5, 0.5, 1.0,
-	0.5, 0.5, -0.5, 1.0,
-	0.5, 0.5, 0.5, 1.0
-	};
+	{ -0.5, -0.5, 0.0, 1.0,
+	-0.5, 0.5, 0.0, 1.0,
+	0.5, -0.5, 0.0, 1.0 };
 	GLfloat vert_colors[] =
 	{ 0.0, 1.0, 1.0, 1.0,
 	1.0, 0.0, 1.0, 1.0,
-	1.0, 1.0, 0.0, 1.0,
-	0.0, 0.0, 1.0, 1.0,
-	0.0, 1.0, 0.0, 1.0,
-	1.0, 0.0, 0.0, 1.0,
-	1.0, 1.0, 1.0, 1.0,
-	0.1, 0.1, 0.1, 1.0 };
-	static const GLushort indices[] = {
-		0, 1, 2, 3, 6, 7, 4, 5,
-		2, 6, 0, 4, 1, 5, 3, 7
-	};
+	1.0, 1.0, 0.0, 1.0 };
+	GLushort indices[] = { 0, 1, 2 };
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glGenBuffers(1, &vbuffer);
@@ -71,55 +34,22 @@ int main(int argc, char* argv[])
 	glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebuffer);
 
-
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) + sizeof(vert_colors), NULL, GL_STATIC_DRAW);
 	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
 	glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(vert_colors), vert_colors);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	GLuint vaos[1];
+	GLuint vap;
 	glGenVertexArrays(1, vaos);
 	glBindVertexArray(vaos[0]);
-
-	GLuint program;
-	ShaderInfo shaders[] =
-	{ { GL_VERTEX_SHADER, "vshader.vert" },
-	{ GL_FRAGMENT_SHADER, "fshader.frag" },
-	{ GL_NONE, NULL } };
-	program = LoadShaders(shaders);
-	glUseProgram(program);
-
-
-
-	GLint posIndex = glGetAttribLocation(program, "vPosition");
-	GLint colIndex = glGetAttribLocation(program, "vColor");
-	GLint scalemIndex = glGetUniformLocation(program, "scale_m");
-	glUniformMatrix4fv(scalemIndex, 1, GL_FALSE, scale);
-	glVertexAttribPointer(posIndex, 4, GL_FLOAT, GL_FALSE, 0, NULL);
-	glVertexAttribPointer(colIndex, 4, GL_FLOAT, GL_FALSE, 0, (GLvoid*) sizeof(vertices));
-	glEnableVertexAttribArray(posIndex);
-	glEnableVertexAttribArray(colIndex);
-
-	glClear(GL_COLOR_BUFFER_BIT);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebuffer);
-	glDrawElements(GL_TRIANGLE_STRIP, 8, GL_UNSIGNED_SHORT, NULL);
-	//glDrawElementsBaseVertex(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, NULL, 0);
-	//glDrawRangeElements(GL_TRIANGLES, 0, 2, 3, GL_UNSIGNED_SHORT, NULL);
-
+	glVertexAttribPointer(vap, 4, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(vap);
+	//glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, NULL);
 	SDL_GL_SwapWindow(window);
-	while (1){
-		SDL_Event event;
-		SDL_PollEvent(&event);
-		if (event.type == SDL_QUIT){
-			glDeleteBuffers(1, &ebuffer);
-			glDeleteBuffers(1, &vbuffer);
-			glDeleteProgram(program);
-			glFlush();
-			SDL_DestroyWindow(window);
-			SDL_Quit();
-			break;
-		}
-	}
+	SDL_Delay(1000);
+	SDL_Quit();
 	return 0;
 }
 
--- drawing1/drawing1/vshader.vert
+++ drawing1/drawing1/vshader.vert
@@ -1,11 +1,1 @@
-#version 430 
-in vec4 vPosition;
-in vec4 vColor;
-uniform mat4 scale_m;
-
-out vec4 fColor;
-
-void main(){
-gl_Position = scale_m*vPosition;
-fColor = vColor;
-}
+#version 430 core
